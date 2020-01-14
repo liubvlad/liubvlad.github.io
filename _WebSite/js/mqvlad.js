11c0792dd3e4c8259cb476ea0186dc73a1f9cf68
Vladislav <47640760+liubvlad@users.noreply.github.com>
2020-01-14 16:44:14 +0300

    MQTT WebSocket Client v0.1

---
+++ _WebSite/js/mqvlad.js
@@ -0,0 +1,14 @@
+var mqtt = require('../node_modules/mqtt');
+var client  = mqtt.connect('mqtt://test.mosquitto.org');
+	client.on('connect', function () {
+  		client.subscribe('presence', function (err) {
+    	if (!err) {
+      		client.publish('presence', 'Hello mqtt');
+    		}
+  		})
+	})
+	client.on('message', function (topic, message) {
+  	// message is Buffer
+  		console.log(message.toString());
+   		client.end();
+	})
